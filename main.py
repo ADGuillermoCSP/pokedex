4fb777cd73ab2714090bee7d598fc56eb57d27f0
ADGuillermoCSP <a24guillermoad@iessanclemente.net>
2026-01-17 17:56:37 +0100

    Integrado menú principal en main.py utilizando funciones del modulo app

--- main.py
+++ main.py
@@ -1,5 +1,35 @@
+from app.funciones import buscar_pokemon, cargar_favoritos, guardar_favorito, eliminar_favorito
+from app.io import mostrar_menu, pedir_opcion
+
+
 def main():
-    print("Pokedex - Proyecto en construcción")
+    """
+    Punto de entrada al programa. Ejecuta un menú interactivo en bucle.
+    """
+    while True:
+        mostrar_menu()
+        opcion = pedir_opcion()
+
+        if opcion == "1":
+            nombre = input("Introduce nombre o ID del Pokémon: ")
+            buscar_pokemon(nombre)  # Aún sin implementar
+
+        elif opcion == "2":
+            cargar_favoritos()  # Aún sin implementar
+
+        elif opcion == "3":
+            id_poke = input("Introduce el ID a eliminar: ")
+            eliminar_favorito(id_poke)  # Aún sin implementar
+
+        elif opcion == "4":
+            print("Saliendo del programa...")
+            break
+
+        else:
+            print("Opcion no válida.")
+
+        input("\nPulsa ENTER para continuar...")
+
 
 if __name__ == '__main__':
     main()
